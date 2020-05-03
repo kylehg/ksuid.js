a88e71f34f26acc4dcfeb3a2268ccee2229aa33f
Kyle Hardgrave <kylehardgrave@gmail.com>
2020-05-03 19:37:53 -0400

    Example test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,10 @@
-import {Greeter} from "./index"
+import {KSUID, KSUID2} from "./index"
 
-test("My Greeter", () => {
-  expect(Greeter("Carl")).toBe("Hello Carl")
+describe("KSUID", () => {
+  it("hex strings are sorted by creation time", () => {
+    const ksuids = [...new Array(100)].map(_ => new KSUID())
+    const hexStrs = ksuids.map(id => id.hex.substr(0, 8))
+    const hexStrsSorted = [...hexStrs].sort()
+    expect(hexStrs).toEqual(hexStrsSorted)
+  })
 })
